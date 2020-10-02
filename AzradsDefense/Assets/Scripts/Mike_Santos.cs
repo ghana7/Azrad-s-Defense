e85221477a77f1fa7e6ff4c7d47cb1c114647a3c
mjs8837 <mjs8837@rit.edu>
2020-10-02 12:10:47 -0400

    Added a script :)
    It's my script B)

---
+++ AzradsDefense/Assets/Scripts/Mike_Santos.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mike_Santos : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int iq = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
