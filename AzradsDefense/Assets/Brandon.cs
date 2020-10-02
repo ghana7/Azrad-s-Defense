4c7d3e648fd9a03fcfcb94dc7356012691c99de4
Brandon Waddill <bwadizle@gmail.com>
2020-10-02 12:19:26 -0400

    added Brandon script

---
+++ AzradsDefense/Assets/Brandon.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brandon : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //HELLLLLLLLLLLLLLLLLLLLLLLLLLP
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
