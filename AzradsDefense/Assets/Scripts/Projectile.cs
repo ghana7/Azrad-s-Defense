9f0c5369a215a7fac69e4738b9a53215c6ba17c2
Jimmie Harkins <ghana796@gmail.com>
2020-10-05 04:00:41 -0400

    Started Shooter script and finished Projectile

---
+++ AzradsDefense/Assets/Scripts/Projectile.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    /// <summary>
+    /// The GameObject that is the target of the projectile
+    /// </summary>
+    private GameObject target;
+    /// <summary>
+    /// The amount of damage dealt by the projectile
+    /// </summary>
+    [SerializeField]
+    private int damage;
+    /// <summary>
+    /// How fast the projectile moves, in units per second
+    /// </summary>
+    [SerializeField]
+    private float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    /// <summary>
+    /// Behavior for when a projectile hits its target
+    /// </summary>
+    protected virtual void Hit()
+    {
+        //Uncomment when health implemented
+        //target.GetComponent<Health>().ChangeHealth(-damage);
+        Debug.Log("hit");
+        Destroy(gameObject);
+    }
+
+    /// <summary>
+    /// Movement behavior for a projectile on each frame
+    /// </summary>
+    protected virtual void Move()
+    {
+        Vector3 displacement = target.transform.position - transform.position;
+        if (displacement.sqrMagnitude <= speed * Time.deltaTime * speed * Time.deltaTime)
+        {
+            Hit();
+        } else
+        {
+            transform.position += displacement.normalized * speed * Time.deltaTime;
+        }
+    }
+}
---
+++ AzradsDefense/Assets/Scripts/Shooter.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject projectilePrefab;
+
+    [SerializeField]
+    private float range;
+
+    [SerializeField]
+    private float shotsPerSecond;
+    
+
+
+    private List<GameObject> targetsInRange;
+    private CircleCollider2D rangeCollider;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+
+    }
+
+    /// <summary>
+    /// Finds the current target to fire at
+    /// 
+    /// Probably should be the furthest forward enemy
+    /// </summary>
+    private void GetTarget()
+    {
+        
+    }
+
+    /// <summary>
+    /// Rotates to face the current target
+    /// </summary>
+    private void Aim()
+    {
+
+    }
+
+    /// <summary>
+    /// Fires a projectile at the current target
+    /// </summary>
+    private void Shoot()
+    {
+
+    }
+
+}
