9951b19a0ede43354417272cefc10764b33ac3d1
Jimmie Harkins <ghana796@gmail.com>
2020-10-06 03:33:29 -0400

    Got range detection working

--- AzradsDefense/Assets/Scripts/Shooter.cs
+++ AzradsDefense/Assets/Scripts/Shooter.cs
@@ -17,29 +17,63 @@ public class Shooter : MonoBehaviour
 
     private List<GameObject> targetsInRange;
     private CircleCollider2D rangeCollider;
-    
 
+    private void Awake()
+    {
+        rangeCollider = GetComponent<CircleCollider2D>();
+
+        targetsInRange = new List<GameObject>();
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        rangeCollider.radius = range;   
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        DebugMove(3);
+        foreach(GameObject g in targetsInRange)
+        {
+            Debug.DrawLine(transform.position, g.transform.position);
+        }
     }
 
-
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void DebugMove(float speed)
     {
-
+        if(Input.GetKey(KeyCode.W))
+        {
+            transform.position += Vector3.up * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += Vector3.down * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
     }
 
-    private void OnCollisionExit2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D other)
     {
+        if(other.gameObject.GetComponent<Health>() != null)
+        {
+            targetsInRange.Add(other.gameObject);
+        }
+    }
 
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.GetComponent<Health>() != null)
+        {
+            targetsInRange.Remove(other.gameObject);
+        }
     }
 
     /// <summary>
