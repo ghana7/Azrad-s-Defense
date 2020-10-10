ecc9f9fe403120d9a7163fcac28ecfed90a9b2d8
Brandon Waddill <bwadizle@gmail.com>
2020-10-10 12:39:33 -0400

    finished Level Manager

--- AzradsDefense/Assets/Scripts/LevelManager.cs
+++ AzradsDefense/Assets/Scripts/LevelManager.cs
@@ -10,38 +10,43 @@ public class LevelManager : MonoBehaviour
     [HideInInspector]
     public Vector3 spawnLocation;
 
-    //WILL NEED A WAY TO STORE MULTIPLE WAVES
-    //this is just temporary to get one wave to work
-    private List<int> waves;
+    private List<List<int>> waves;
 
     private int wavesSpawned;
     
-
     // Start is called before the first frame update
     void Start()
     {
         spawnLocation = new Vector3(0, 0, 0);
         wavesSpawned = 0;
-        waves = new List<int> { 0, 0, 0, 0, 0 };
-        //waves.Push(new List<int>[0, 0, 0, 0, 0]);
-        //waves.Push(new List<int>[0, 0]);
+        //waves = new List<int> { 0, 0, 0, 0, 0 };
+        waves = new List<List<int>>();
+        HardcodeWave();
         instance = this;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //if(WaveManager.instance.Completed() && wavesSpawned < waves.Count)
-        if(WaveManager.instance.Completed())
+        if(WaveManager.instance.Completed() && wavesSpawned < waves.Count)
         {
             SendWave();
+        }
+        if(WaveManager.instance.Completed() && wavesSpawned == waves.Count)
+        {
+            //END THE LEVEL
         }
     }
 
     private void SendWave()
     {
-        //WaveManager.instance.enemiesToSpawn = waves[wavesSpawned];
-        WaveManager.instance.enemiesToSpawn = waves;
+        WaveManager.instance.enemiesToSpawn = waves[wavesSpawned];
         wavesSpawned++;
+    }
+
+    private void HardcodeWave()
+    {
+        waves.Add(new List<int> { 0, 0, 0, 0, 0 });
+        waves.Add(new List<int> { 0, 0 });
     }
 }
