4c8f149c3b40f77b71f16c70cf3d8312a23dc13b
Jimmie Harkins <ghana796@gmail.com>
2020-10-12 19:31:55 -0400

    AOE ships and range indicators

---
+++ AzradsDefense/Assets/Scripts/AOEShooter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AOEShooter : Shooter
+{
+    [SerializeField]
+    private GameObject particleWrapper;
+
+    public override void Shoot(int index)
+    {
+        for (int i = 0; i < targetsInRange.Count; i++)
+        {
+            projectilePrefabs[index].GetComponent<Projectile>().Hit(targetsInRange[i]);
+        }
+        particleWrapper.GetComponent<ParticleSystem>().Play();
+    }
+
+    protected override void Aim()
+    {
+
+    }
+}
