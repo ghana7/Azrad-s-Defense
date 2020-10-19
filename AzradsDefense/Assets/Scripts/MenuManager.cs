dbb234a11c7aeabdee5680c9ffb98a93b07d84d8
Brandon Waddill <bwadizle@gmail.com>
2020-10-19 18:59:42 -0400

    menu working

---
+++ AzradsDefense/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject menu;
+    public GameObject levelSelector;
+    public int levelToGoTo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void EnableDisableLevelSelector()
+    {
+        menu.SetActive(!menu.activeSelf);
+        levelSelector.SetActive(!menu.activeSelf);
+    }
+
+    public void ExitGame()
+    {
+        //DOES NOT WORK IN EDITOR (needs to be tested during build)
+        Debug.Log("READ -> The Application.Quit() function does not work in the editor and must be tested after building");
+        Application.Quit();
+    }
+
+    public void GoToLevel()
+    {
+        string level = levelToGoTo.ToString();
+        level = string.Concat("level", level);
+        //Debug.Log("Going to '" + level + "'");
+        SceneManager.LoadScene(level);
+    }
+}
