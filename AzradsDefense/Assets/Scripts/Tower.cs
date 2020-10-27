64e19f35457b3d4e48807e541810cfcce0f6f1a2
Anthony <ajl3717@rit.edu>
2020-10-26 20:09:37 -0400

    Fixed cannon ship bug

--- AzradsDefense/Assets/Scripts/Tower.cs
+++ AzradsDefense/Assets/Scripts/Tower.cs
@@ -74,7 +74,7 @@ public class Tower : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (isPlaced != false)
+        if (isPlaced != false && canTravel == true)
         {
             if (firstPointPlaced == true && secondPointPlaced == false)
             {
